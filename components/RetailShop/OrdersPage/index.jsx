aeacf2f1fcc0e3a95f72927f25009d06a25e48f9
Veeraiya, Manikandan <manibicsglobal@gmail.com>
2024-06-06 11:18:10 +0530

    Fix width for table in orders page

--- components/RetailShop/OrdersPage/index.jsx
+++ components/RetailShop/OrdersPage/index.jsx
@@ -36,7 +36,7 @@ const OrdersPage = () => {
 
   return (
     <div className=" bg-white p-4">
-      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <div className=" mx-auto py-6 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center mb-4">
           <h1 className="text-3xl font-bold text-gray-900">My Orders</h1>
           <input
